c90c926880fa903fd65eee06e8ee1a23bfc44680
maxonagu <109534933+maxonagu@users.noreply.github.com>
2023-02-13 12:38:21 +0300

    add all this files

---
+++ index.js
@@ -0,0 +1,18 @@
+//document.getElementById("count-el").innerText = 6
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment(){
+    count += 1;
+    countEl.textContent = count;
+    console.log(count);
+}
+
+function save(){
+    let countstr = count + " - "
+    saveEl.textContent += countstr
+    countEl.textContent = 0
+    count = 0 
+}
